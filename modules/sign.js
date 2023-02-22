47eb1a60f3893973502e1fb0f3e9962fed0c9ccc
Bakhrulbek <85066342+bakhrulbek-arch@users.noreply.github.com>
2023-02-22 16:14:14 +0500

    update

---
+++ modules/sign.js
@@ -0,0 +1,11 @@
+let body = document.querySelector('body');
+let arr = () => {
+    let block_sign = document.createElement('div')
+    block_sign.classList.add('block_sign')
+
+    alert('yes')
+    body.append(block_sign)
+}
+
+
+// export default arr
