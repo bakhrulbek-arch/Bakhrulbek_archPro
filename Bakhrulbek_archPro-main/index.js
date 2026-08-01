ed6525276105f85ab509844ed6a0492dec8d13cd
Bakhrulbek <85066342+bakhrulbek-arch@users.noreply.github.com>
2026-08-01 21:23:29 +0500

    Delete Bakhrulbek_archPro-main directory

--- Bakhrulbek_archPro-main/index.js
+++
@@ -1,9 +0,0 @@
-import arr from'./modules/sign.js'
-// import { arr } from "./modules/sign";
-let sing_in = document.querySelectorAll('.sign_in')
-sing_in.forEach(i => {
-    i.onclick = () => {
-        arr()
-    }
-});
-arr()
